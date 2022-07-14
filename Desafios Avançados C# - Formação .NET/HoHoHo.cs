c90402e7abef58fb229b3277adb025d3bd8aeb02
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-07-14 12:46:11 -0300

    Create HoHoHo.cs

---
+++ Desafios Avançados C# - Formação .NET/HoHoHo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _52._36_URI_1759 {
+  class Program {
+    static void Main(string[] args) {
+      int N = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i < N-1; i++) {
+        Console.Write("Ho ");
+      }
+      Console.WriteLine("Ho!");
+    }
+  }
+}
